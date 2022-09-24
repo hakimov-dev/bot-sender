4ac24efb43b037d7d4e26b56b9def385a04f57c5
Muhammadamin <webhacker0100@gmail.com>
2022-09-24 16:00:43 +0500

    Deleted exes items and update

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const api = require('./api')
 
-async function senMessage(TOKEN, CHAT, message){
-  try{
+function checkMessage(TOKEN, CHAT, message){
     if(TOKEN !== undefined && TOKEN.length && CHAT !== undefined && CHAT.length && message !== undefined && message.length > 0){
-        api.get(`bot${TOKEN}/sendMessage?chat_id=${CHAT}}&text=${message}`)
-        .then(res => {
-            console.log('Message successfully send.')
-        })
-        .catch(error => {
-            console.log(error)
-        })
+       
     }else{
         console.error('Added function params!')
     }
-  }catch(error){
-    console.log(error)
-  }
-}
+};
 
-module.exports = senMessage
+module.exports = checkMessage
