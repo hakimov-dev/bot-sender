b69b41c5ee4b7a32db92b3df1975a73c613a75a1
Muhammadamin <webhacker0100@gmail.com>
2022-09-24 14:53:44 +0500

    Update gitignore

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const api = require('./api')
 async function senMessage(TOKEN, CHAT, message){
   try{
     if(TOKEN !== undefined && TOKEN.length && CHAT !== undefined && CHAT.length && message !== undefined && message.length > 0){
-        api.get(`bot/${TOKEN}/sendMessage?chat_id=${CHAT}$text=${message}/`)
+        api.get(`bot${TOKEN}/sendMessage?chat_id=${CHAT}}&text=${message}`)
         .then(res => {
             console.log('Message successfully send.')
         })
